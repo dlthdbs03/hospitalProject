a8399294e627953904bf34345b236cd9e0f58a80
HeeYeon-Kim <khyeon2020@naver.com>
2022-11-15 17:41:08 +0900

    chore: txt 파일 읽기 기능 추가
    txt파일 읽고 Manager 클래스 readAll이용해 각각의 Manager의 mList에 txt파일
    내용 저장할 수 있도록 함.

--- src/hospital/Main.java
+++ src/hospital/Main.java
@@ -14,14 +14,36 @@ public class Main {
 			hospitalMain = new Main();
 		return hospitalMain;
 	}
-	Scanner scan = new Scanner(System.in); 
+	Scanner scan = new Scanner(System.in);
 	static Manager doctorMgr = new Manager();
 	static Manager patientMgr = new Manager();
+	static Manager receptionMgr = new Manager();
 	public void run()
 	{
-		
+		doctorMgr.readAll("doctor.txt",new Factory(){
+			public Manageable create(){
+				return new Doctor();
+			}
+		});
+		System.out.println("\n================= 전체 의사 리스트 =================");
+		doctorMgr.printAll();
+		patientMgr.readAll("patient.txt",new Factory(){
+			public Manageable create(){
+				return new Patient();
+			}
+		});
+
+		receptionMgr.readAll("reception.txt",new Factory(){
+			public Manageable create(){
+				return new Reception();
+			}
+		});
+		System.out.println("\n================= 전체 환자 리스트 =================");
+		patientMgr.printAll();
+		System.out.println("\n=============== 전체 접수 리스트 =================");
+		receptionMgr.printAll();
 	}
-	
+
 	public static void main(String args[]) {
 		Main m = new Main();
 		m.run();
