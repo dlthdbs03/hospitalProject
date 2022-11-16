f462dae2cca94798ab115502eeae09e8d0a7fc4e
Jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2022-11-16 16:25:14 +0900

    Update Reservation.java

--- src/hospital/Reservation.java
+++ src/hospital/Reservation.java
@@ -1,56 +1,53 @@
 package Hospital;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import facade.DataEngineInterface;
-import mgr.Factory;
 import mgr.Manageable;
-import mgr.Manager;
 
-public class ReservationMgr {
-	public class ItemMgr implements DataEngineInterface {
-		private static ItemMgr mgr = null;
-		private ItemMgr() {}
-		public static ItemMgr getInstance() {
-			if (mgr == null)
-				mgr = new ItemMgr();
-			return mgr;
+public class Reservation extends Reception{
+		ArrayList <Vaccination> typeList = new ArrayList<>();
+		public void setType(Vaccination v) {  //예방접종 종류 리스트 복사
+			typeList = v.typeList;
+		}
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		static List<String> getStringList(Scanner scan) { // 날짜랑 정보 같이 입력받음, 예약리스트에 저장
+			List<String> reserveList = new ArrayList<>();
+			String tmp;
+			while (true) {
+				tmp = scan.next();
+				if (tmp.contentEquals("-1"))
+					break;
+				super.read(scan); 
+				reserveList.add(tmp);
+			}
+			return reserveList;
 		}
 		
-		private String[] headers = {"날짜", "이름", "나이", "성별", "증상", "의사"};
-		@Override
-		public String[] getColumnNames() {
-			return headers;
+		public search(int kwd) { //날짜 선택해서 해당 날짜 현황 보여줌
+			if (date==kwd) {
+				for( String s : reserveList ) 
+					if((""+kwd).contentEquals(s))
+						return reserveList;
+			}
 		}
-		@Override
-		public int getColumnCount() {
-			// TODO Auto-generated method stub
-			return 6;
+		
+		public void addReserve(int kwd) {
+			// 예약 버튼 누르면 구현되도록 GUI작성
 		}
-		@Override
-		public void readAll(String filename) {
-			// TODO Auto-generated method stub
-		}
-		@Override
-		public List<Manageable> search(String kwd) {
-			// TODO Auto-generated method stub
-			return Main.ReservationMgr.findAll(kwd);
-		}
-		@Override
-		public void addNewItem(String[] uiTexts) {
-			// TODO Auto-generated method stub
-		}
-		@Override
-		public void update(String[] uiTexts) {
-			// TODO Auto-generated method stub
-			
-		}
-		@Override
-		public void remove(String kwd) {
-			// TODO Auto-generated method stub
-
-		}
-	}
+		
+		public boolean matches(String kwd) {
+			if(super.matches(kwd)) //진료내역 매치됨
+				return true;
+			if(!super.symptom.contentEquals(kwd)) { //증상이 kwd와 맞지 않음
+				while(true){
+					for(Vaccination v : typeList) //예방접종 종류 리스트를 가져와 kwd와 비교
+						if(v.matches(kwd))
+							return true;
+				}
+			}
+			return false;
+	    }
 }
